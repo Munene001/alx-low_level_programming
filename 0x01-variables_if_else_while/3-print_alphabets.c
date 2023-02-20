8f965517805450fe9e24d2a79ea45b7a30c22b2f
Your Name <you@example.com>
2023-02-20 12:58:03 -0800

    3z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {	
-	char ALP[26] = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"
+	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
