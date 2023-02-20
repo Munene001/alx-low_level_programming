512f75d5255d8de7f596b44a92b1394104c91f6e
Your Name <you@example.com>
2023-02-20 13:04:18 -0800

    3z

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,20 +6,16 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{	
-	char ALP[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
-	for (i = 0; i < 26; i++)
-	{
-		putchar(ALP[i]);
-	}	
 	putchar('\n');
 	return (0);
 }
+
 
