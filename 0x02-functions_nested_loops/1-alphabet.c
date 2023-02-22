cd78c69da1ccfe678a8e7b4483779b2dfe296a03
Your Name <you@example.com>
2023-02-22 07:11:50 -0800

    2xxx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,26 +2,25 @@
 #include "main.h"
 
 
-	/**
-	 * main - check the code
-	 * print_alphabet - print all alphabet in lowercases
-	 *
-	 * Description: 'void print_alphabet(void)- This function prints the lowercase alphabet, from 'a' to 'z','
-	 * Return: Always 0.
-	 */
+/**
+ * main - check the code
+ * print_alphabet - print all alphabet in lowercases
+ *
+ * Return: Always 0.
+ */
 
 
 
 
-	void print_alphabet(void)
-	{
-		char letter;
+void print_alphabet(void)
+{
+	char letter;
 
 
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
 
 		_putchar('\n');
-	}
+}
 
