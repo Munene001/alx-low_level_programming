ecf7a46482135a005f11cacd03a16c109b291b80
Your Name <you@example.com>
2023-02-22 07:52:39 -0800

    vexx

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include<stdio.h>
+
+
 /**
  * 0-putchar.c - prints the string "_putchar" to stdout
  * main - Entry point, prints the string "_putchar" to stdout
  * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 	char str[] = "_putchar";
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -25,7 +25,8 @@ void print_alphabet_x10(void)
 		}
 			_putchar('\n');
 			i++;
-		}
 	}
+	return (0);
+}
 
 
