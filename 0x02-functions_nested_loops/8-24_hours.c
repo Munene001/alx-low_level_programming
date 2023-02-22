823888e207f80a329263e88eebd182bb4e99a812
Your Name <you@example.com>
2023-02-22 13:44:26 -0800

    8xxxxxx

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
  */
 void jack_bauer(void)
 {
-	int i,j;
+	int i, j;
 	i = 0;
 
 	while (i < 24)
 	{
 		j = 0;
-		while (j > 60)
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			_putchar(':');
+			_putchar(':')
 			_putchar((j / 10) + '0');	
 			_putchar((j % 10) + '0');
 			_putchar('\n');
