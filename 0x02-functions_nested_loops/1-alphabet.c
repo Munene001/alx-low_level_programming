fd1e13e5ce9a55321cb183e24609ccd8ef20314f
Your Name <you@example.com>
2023-02-22 08:00:40 -0800

    vexxxx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include<unistd.h>
+#include<stdio.h>
 
 
 /**
  * main - check the code
  * print_alphabet - print all alphabet in lowercases
- *
- * Return: Always 0.
+ *Return: Always 0.
  */
 void print_alphabet(void)
 {
