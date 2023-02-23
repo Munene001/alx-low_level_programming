626f0360c5c190a9fb227ee99134e0e5e4fc0cb8
Your Name <you@example.com>
2023-02-23 11:46:28 -0800

    3yyy

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,13 @@
-#ifndef main_h
-#define main_h
+#ifndef MAIN_h
+#define MAIN_h
+
+#include<stdio.h>
 
 void positive_or_negative(int i);
-int main(void);
 int largest_number(int a, int b, int c);
+void print remaining days(int month, int day, int year);
 int convert_day(int month, int day);
+void print_times_table(int n);
 
 
 
