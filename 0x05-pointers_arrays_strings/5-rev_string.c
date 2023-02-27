1491b8b144b6ff4457048085be1ce9b7efa8a135
Your Name <you@example.com>
2023-02-27 15:37:29 -0800

    5x

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,18 +8,21 @@
 
 void rev_string(char *s)
 {
-	char rev = s[0];
 	int counter = 0;
-	int i;
+	int i, max_index;
+	char rev[counter];
 
 	while (s[counter] != '\0')
 	{
 		counter++;
 	}
+	max_index = counter - 1;
 	for (i = 0; i < counter; i++)
 	{
-		counter--;
-		rev = s[i];
-		s[counter] = rev;
+		rev[i] = s[max_index - i];
+	}
+	for (i = 0; i < counter; i++)
+	{
+		s[i] = rev[i];
 	}
 }
