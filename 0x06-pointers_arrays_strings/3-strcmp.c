1087ba4c4ce759e6e651674d758f45130b644a16
Your Name <you@example.com>
2023-03-02 06:21:53 -0800

    3x

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp - unction that compares two strings.
- * @s1: first variable
- * @s2: second variable
- * return: s1[i] - s2[i]
+ * _strcmp - function that compares two strings.
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[i] - s2[i]
  */
 int _strcmp(char *s1, char *s2)
 {
