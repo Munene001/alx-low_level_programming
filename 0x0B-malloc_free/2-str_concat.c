a96c07329b2da13e6406e2215d4c03587f218251
Your Name <you@example.com>
2023-03-13 05:31:42 -0700

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -34,6 +34,11 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	i = ci = 0;
+	while (s1[i] != '\0')
+	{
+		conct[i] = s1[i];
+		i++;
+	}
 	while (s2[ci] != '\0')
 	{
 		conct[i] = s2[ci];
